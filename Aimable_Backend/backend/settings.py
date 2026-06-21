3d1d085453124ed5df8f83324af654754540f532
emmykalisa2025-hub <emmykalisa2025@gmail.com>
2026-06-21 20:13:56 +0200

    Update settings.py

--- Aimable_Backend/backend/settings.py
+++ Aimable_Backend/backend/settings.py
@@ -120,12 +120,19 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# STORAGES = {
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
+"default": {
+"BACKEND": "django.core.files.storage.FileSystemStorage",
+},
+"staticfiles": {
+"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+},
 }
-
 
 # Media files
 MEDIA_URL = "/media/"
